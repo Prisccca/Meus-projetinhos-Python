de72a6128f395850db1a78a7161b282b1f63d3c8
Priscilla Falcão <priscillaffd@gmail.com>
2024-10-02 10:26:44 -0300

    Programa 01

---
+++ Estrutura Sequencial/_01.py
@@ -0,0 +1,10 @@
+#Faça um programa que receba 4 numeros inteiros , calcule e mostre a soma desses números
+
+print ("Bem Vindo ao Soma 4: Digite quatro numeros para somar")
+x = int(input ("Digite o primeiro numero"))
+y = int(input ("Digite o segundo numero"))
+z = int(input ("Digite o terceiro numero"))
+w = int(input ("Digite o quarto numero"))
+
+soma = x + y + z + w
+print(soma)
